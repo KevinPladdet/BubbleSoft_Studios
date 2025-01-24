cd1dc485ad1322db4c57f21adabb816479300a1f
KevinPladdet <91886057+KevinPladdet@users.noreply.github.com>
2025-01-24 22:09:16 +0100

    Start to GameManager and BubbleMovement

---
+++ BubbleSoft/Assets/Kevin/Scripts/BubbleBehaviour.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleBehaviour : MonoBehaviour
+{
+    void Start()
+    {
+        // Tomorrow: make it so that the bubbles spawn outside of "WaterArea" and that they slowly go towards the player
+        // (eventually make it so that they start fast and go slower the closer they get to the player)
+        
+        // Also make a trigger on the player that checks for the "Bubble" tag and if it collides the gameobject/bubble that touched the trigger gets deleted
+    }
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ BubbleSoft/Assets/Kevin/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    // Difficulty Values
+    [SerializeField] private int bubbleSpeed;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
