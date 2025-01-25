f90f2e8f71609ceabc1503be761de1a7ac14808a
Daniel Castillo <me@4az.dev>
2025-01-25 20:19:52 +0100

    WIP: Bubble chained explosions

--- BubbleSoft/Assets/Kevin/Scripts/BubbleBehaviour.cs
+++ BubbleSoft/Assets/Kevin/Scripts/BubbleBehaviour.cs
@@ -11,6 +11,7 @@ public class BubbleBehaviour : MonoBehaviour
     [SerializeField] private float speedMultiplier;
     [SerializeField] private int health;
     [SerializeField] private int damage;
+    [SerializeField] private CircleCollider2D chainCollider;
 
     private Transform playerPos;
     private GameManager gm;
@@ -18,11 +19,15 @@ public class BubbleBehaviour : MonoBehaviour
 
     public BubbleConfig bubbleConfig;
 
+    [SerializeField] private List<BubbleBehaviour> chainedBubbles;
+    private bool isBeingDestroyed = false;
+
     void Start()
     {
         playerPos = GameObject.Find("Player").transform;
         gm = GameObject.Find("GameManager").GetComponent<GameManager>();
 
+        chainedBubbles = new List<BubbleBehaviour>();
         GetComponent<SpriteRenderer>().sprite = bubbleConfig.sprite;
         health = bubbleConfig.health;
         damage = bubbleConfig.damage;
@@ -40,16 +45,53 @@ public class BubbleBehaviour : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log($"Collision started with: {collision.gameObject.name}");
-        var bullet = collision.gameObject;
+        //Debug.Log($"Collision started with: {collision.gameObject.name}");
+        var collisionObject = collision.gameObject;
 
-        Destroy(bullet);
-
-        health -= 1;
-
-        if (health < 1)
+        if (collisionObject.tag == "Bubble")
         {
-            Destroy(gameObject);
+            Debug.Log("Touching");
+            chainedBubbles.Add(collisionObject.GetComponent<BubbleBehaviour>());
+
+        } else if (collisionObject.tag == "Bullet")
+        {
+            Destroy(collisionObject);
+
+            health -= 1;
+
+            if (health < 1)
+            {
+                Bullet bullet = collisionObject.GetComponent<Bullet>();
+                destroyBubble(bullet.type);
+            }
+
+        }
+
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        var collisionObject = collision.gameObject;
+        Debug.Log("Removing object");
+        if (collisionObject.tag == "Bubble")
+        {
+            chainedBubbles.Remove(collisionObject.GetComponent<BubbleBehaviour>());
+        }
+    }
+
+    private void destroyBubble(ColorType type)
+    {
+        Debug.Log("Deleting {gameObject.name}");
+        if (isBeingDestroyed) return;
+        isBeingDestroyed = true;
+
+        Debug.Log("List chainedBubbles length: {chainedBubbles.Length}");
+        Destroy(this.gameObject);
+        foreach (var bubble in chainedBubbles)
+        {
+            
+            bubble.destroyBubble(type);
         }
 
     }
